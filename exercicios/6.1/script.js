8369ec4d3d0919cdf1ea3e82667c6448ecd2ccc2
Mateus Talles <mateustalles@gmail.com>
2019-10-14 18:00:53 -0300

    Parte I completa.

---
+++ exercicios/6.1/script.js
@@ -0,0 +1,96 @@
+window.addEventListener("load", function() {
+    adicionaEstados();
+});
+
+function adicionaEstados() {
+let campoEstados = document.getElementById("campo-estados");
+let listaEstados = [{
+    name: "Acre",
+    value: "ac"
+}, {
+    name: "Alagoas",
+    value: "al"
+}, {
+    name: "Amapá",
+    value: "ap"
+}, {
+    name: "Amazonas",
+    value: "am"
+}, {
+    name: "Bahia",
+    value: "ba"
+}, {
+    name: "Ceará",
+    value: "ce"
+}, {
+    name: "Distrito Federal",
+    value: "df"
+}, {
+    name: "Espírito Santo",
+    value: "es"
+}, {
+    name: "Goiás",
+    value: "go"
+}, {
+    name: "Maranhão",
+    value: "ma"
+}, {
+    name: "Mato Grosso",
+    value: "mt"
+}, {
+    name: "Mato Grosso do Sul",
+    value: "ms"
+}, {
+    name: "Minas Gerais",
+    value: "mg"
+}, {
+    name: "Pará",
+    value: "pa"
+}, {
+    name: "Paraíba",
+    value: "pb"
+}, {
+    name: "Paraná",
+    value: "pr"
+}, {
+    name: "Pernambuco",
+    value: "pe"
+}, {
+    name: "Piauí",
+    value: "pi"
+}, {
+    name: "Rio de Janeiro",
+    value: "rj"
+}, {
+    name: "Rio Grande do Norte",
+    value: "rn"
+}, {
+    name: "Rio Grande do Sul",
+    value: "rs"
+}, {
+    name: "Rondônia",
+    value: "ro"
+}, {
+    name: "Roraima",
+    value: "rr"
+}, {
+    name: "Santa Catarina",
+    value: "sc"
+}, {
+    name: "São Paulo",
+    value: "sp"
+}, {
+    name: "Sergipe",
+    value: "se"
+}, {
+    name: "Tocantins",
+    value: "to"
+}];
+    for (let estado of listaEstados) {
+        let criaEstado = document.createElement("option");
+        criaEstado.innerText = estado.name;
+        criaEstado.value = estado.value;
+        campoEstados.appendChild(criaEstado);
+    }
+}
+
