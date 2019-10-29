226fbc2f3a4d469a46f6ef04c65f706bacce1819
Mateus Talles <mateustalles@gmail.com>
2019-10-29 09:22:08 -0300

    Ex2_4_Função 2

--- exercicios/8.1/ex.2.4.js
+++ exercicios/8.1/ex.2.4.js
@@ -1,7 +1,16 @@
-const wheresX = (string) => {
+function wheresX (string) {
     const paramString = `Tryber x aqui!
 
     Tudo bem?`
     return paramString.replace('x', string)
 }
-console.log(wheresX("Wally"))
+
+const stringSkills = [' Fluent English', ' Assertive Communication', ' Logical Thinking', ' Dynamism',' Hard Work']
+
+const firstStr = wheresX("Wally");
+function newString(str) {
+    return `${str}
+            
+${stringSkills.sort()}`
+}
+console.log(newString(firstStr));
