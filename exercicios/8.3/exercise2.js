5f539557039aaed4696572d338c218961eb53613
Mateus Talles <mateustalles@gmail.com>
2019-10-30 18:47:49 -0300

    Exercicio 2 - usando reduce

--- exercicios/8.3/exercise2.js
+++ exercicios/8.3/exercise2.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
 
 // escreva sum abaixo
-const sum = ( a = 0, ...args) => parseInt(a) + parseInt(args) ;
-console.log (sum(1,2))
+// const sum = ( a = 0, ...args) => parseInt(a) + parseInt(args) ;
+
+const sum = (...args) => args.reduce( (a , b)  => a + b , 0 );
 
 assert.equal(sum(), 0)
 assert.equal(sum(1), 1)
