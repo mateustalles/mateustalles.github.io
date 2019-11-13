139caf051372c390887106c97902796fa48e295f
Mateus Talles <mateustalles@gmail.com>
2019-11-13 18:29:21 -0300

    Ex. 3 done

--- exercicios/10.1/nome-app/src/App.js
+++ exercicios/10.1/nome-app/src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 
 function App() {
   const taskList = ['Um, dois, feijão com arroz', 'Três,quatro, feijão no prato', 'Cinco, seis, falar inglẽs', 'Sete, oito, comer biscoito', 'Nove, dez, comer pasteis']
+  const JSX = <div>
+<h1>Hello, Im JSX</h1>
+<p>This goes a long description about me</p>
+<ul><li>This</li>
+<li>now you know</li>
+<li>All right</li>
+</ul>
+</div>
+
   return (<div>
     <h1>Lista de tarefas</h1>
-<ol>{taskList.map(task => <li>{task}</li>)}</ol>
+    <ol>{taskList.map(task => <li>{task}</li>)}</ol>
+    {JSX}
   </div>
   );
+  
 }
 
 export default App;
