bde60d05a4c1b61fd3d9af41fbcfd8b99eddb950
Mateus Talles <mateustalles@gmail.com>
2019-11-18 13:01:54 -0300

    Installing Bootstrap

--- exercicios/10.2/my-pokedex/src/Pokedex.js
+++ exercicios/10.2/my-pokedex/src/Pokedex.js
@@ -2,12 +2,17 @@ import pokemons from './data'
 import React from 'react';
 import Pokemon from './Pokemon'
 import './Pokedex.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Pokedex extends React.Component {
   render() {
   return (
-    <div className="pokedex">
-      {pokemons.map(pokemon =><Pokemon key={pokemon.id} pokeName={pokemon.name}/>)}
+    <div className="container">
+      <div className="row">
+        <div className="d-flex flex-row p-2 bd-highlight">
+          {pokemons.map(pokemon =><Pokemon key={pokemon.id} pokeName={pokemon.name}/>)}
+        </div>
+      </div>
     </div>
   )
 
