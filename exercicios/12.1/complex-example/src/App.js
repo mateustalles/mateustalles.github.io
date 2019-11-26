bc7e7aca977ba685c66dad968d70a74da04ff200
Mateus Talles <mateustalles@gmail.com>
2019-11-26 14:57:20 -0300

    Hum

--- exercicios/12.1/complex-example/src/App.js
+++ exercicios/12.1/complex-example/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Picture from './Picture';
+import Button from './Button';
+class App extends Component {
+  constructor(props) {
+    super(props);
+this.state = {
+      pictures: [
+        {id: 1, src: 'http://via.placeholder.com/200x100'},
+        {id: 2, src: 'http://via.placeholder.com/400x200'},
+        {id: 3, src: 'http://via.placeholder.com/200x100'}
+      ],
+      currentPic: null
+    };
+this.setCurrentPic = this.setCurrentPic.bind(this);
+  }
+setCurrentPic(id) {
+    this.setState({currentPic: id});
+  }
+render () {
+    return (
+      <div>
+        <div className='squares'>
+          {this.state.pictures.map((picture) => {
+            return (
+              <Picture key={picture.id} src={picture.src}>
+                <Button
+                  pictureSrc={picture.src}
+                  setCurrentPic={this.setCurrentPic}
+                  id={picture.id}
+                />
+              </Picture>
+            )
+          })}
+        </div>
+        <div>
+          <p>Current selected picture ID is {this.state.currentPic}</p>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
