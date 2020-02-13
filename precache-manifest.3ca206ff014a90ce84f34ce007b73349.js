a558d98602f8175fed4d83d8824ff626f9755149
Mateus Talles <mateustalles@gmail.com>
2020-02-13 20:05:50 -0300

    Updates

---
+++ precache-manifest.3ca206ff014a90ce84f34ce007b73349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a095d697d26bc670e298418929837e5f",
+    "url": "/projetos/movie-card-library/index.html"
+  },
+  {
+    "revision": "92f498379719206a6bae",
+    "url": "/projetos/movie-card-library/static/css/main.4fd9617b.chunk.css"
+  },
+  {
+    "revision": "11c77c47fc9c608505ac",
+    "url": "/projetos/movie-card-library/static/js/2.2c839db7.chunk.js"
+  },
+  {
+    "revision": "92f498379719206a6bae",
+    "url": "/projetos/movie-card-library/static/js/main.10e2dac7.chunk.js"
+  },
+  {
+    "revision": "4b83cca354263523e6c8",
+    "url": "/projetos/movie-card-library/static/js/runtime-main.6d70bd92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projetos/movie-card-library/precache-manifest.5d840eaea84ad2760a7df8839f37bd27.js"
+  "/projetos/movie-card-library/precache-manifest.3ca206ff014a90ce84f34ce007b73349.js"
 );
 
 self.addEventListener('message', (event) => {
