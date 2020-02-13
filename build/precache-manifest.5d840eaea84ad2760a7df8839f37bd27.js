174edb2444a748d7185cb95e367e309097bf9b53
Mateus Talles <mateustalles@gmail.com>
2020-02-13 19:59:20 -0300

    Deploy

---
+++ build/precache-manifest.5d840eaea84ad2760a7df8839f37bd27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba283eac9e650e95eeb89fafec3699f8",
+    "url": "/projetos/movie-card-library/index.html"
+  },
+  {
+    "revision": "81055861a5b3c8b7a1c8",
+    "url": "/projetos/movie-card-library/static/css/main.c5604e8d.chunk.css"
+  },
+  {
+    "revision": "bc37a35819db8c85d74d",
+    "url": "/projetos/movie-card-library/static/js/2.9b1ef554.chunk.js"
+  },
+  {
+    "revision": "81055861a5b3c8b7a1c8",
+    "url": "/projetos/movie-card-library/static/js/main.2119e748.chunk.js"
+  },
+  {
+    "revision": "4b83cca354263523e6c8",
+    "url": "/projetos/movie-card-library/static/js/runtime-main.6d70bd92.js"
+  }
+]);
