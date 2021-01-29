e6df873c7dd362787b876b2b8a98d58c2b443e2d
Mateus Talles <mateustalles@gmail.com>
2021-01-29 01:43:51 -0300

    added legibility perks

--- hash_maps/reconstructing_queue/reconstructing_queue.py
+++ hash_maps/reconstructing_queue/reconstructing_queue.py
@@ -9,19 +9,19 @@ def reconstructing_queue(
     for prev_person, next_person in people:
         left.append(prev_person)
         right.append(next_person)
-        queue[prev_person] = [next_person, 0]
+        queue[prev_person] = {"name": next_person, "count": 0}
 
     for _, next_person in people:
         if queue.get(next_person):
-            queue[next_person][1] += 1
+            queue[next_person]["count"] += 1
 
-    next_person = [person for person in left if queue[person][1] == 0][0]
+    next_person = [person for person in left if queue[person]["count"] == 0][0]
     # this is actually the first person of the queue
 
     ordered_queue = [next_person]
     while queue.get(next_person):
-        ordered_queue.append(queue[next_person][0])
-        next_person = queue[next_person][0]
+        ordered_queue.append(queue[next_person]["name"])
+        next_person = queue[next_person]["name"]
 
     return ordered_queue
 
