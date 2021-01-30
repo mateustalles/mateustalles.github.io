fcee4ac3d48ba9ddaf0c55440149d7fcaa0ed1b9
Mateus Talles <mateustalles@gmail.com>
2021-01-30 18:30:16 -0300

    Follow-up 2: adds new personal and interpolates groups find the ones not at max capacity

--- hash_maps/interview_questions/ex_google.py
+++ hash_maps/interview_questions/ex_google.py
@@ -28,7 +28,8 @@
 
 class SubordinatesRank:
     def __init__(self):
-        self._subordinates = {
+        self.personnel = [1, 2, 4, 5, 6, 7]
+        self._subordinates_rank = {
             1: [[2, 3], []],
             2: [[4], []],
             3: [[], []],
@@ -37,6 +38,7 @@ class SubordinatesRank:
             6: [[], []],
             7: [[], []]
         }
+        self._max_per_team = 3
 
     def calc_score(self, id, score=1, iterated_ids=[]):
         iterated_ids.append(id)
@@ -48,27 +50,79 @@ class SubordinatesRank:
         # - 6 => sem subordinados
         # - 7 => sem subordinados
 
-        if id > len(self._subordinates):
-            self._subordinates[iterated_ids[0]][1] = score
+        if id > len(self._subordinates_rank):
+            self._subordinates_rank[iterated_ids[0]][1] = score
             return score
 
-        if self._subordinates[id][1]:
-            score = self._subordinates[id][1]
+        if self._subordinates_rank[id][1]:
+            score = self._subordinates_rank[id][1]
             return score
 
-        if score == 1 and not self._subordinates[id][0]:
-            self._subordinates[id][1] = 0
+        if score == 1 and not self._subordinates_rank[id][0]:
+            self._subordinates_rank[id][1] = 0
             return 0
 
-        if self._subordinates[id][0] != None:
-            score += len(self._subordinates[id][0])
+        if self._subordinates_rank[id][0] != None:
+            score += len(self._subordinates_rank[id][0])
             id += 1
             return self.calc_score(id, score, iterated_ids)
+
+        
+    def add_subordinate(self, id, team):
+        if id in self.personnel:
+            raise ValueError("ID já existe")
+        else:
+            self.personnel.append(id)
+
+        while len(self._subordinates_rank[team][0]) >= self._max_per_team:
+            team += 1
+
+        self._subordinates_rank[team][0].append(id)
+        print(f'Adicionado ID {id} no time {team} (time pode haver mudado devido limitações na capacidade')
+
 
 if __name__ == "__main__":
     rank = SubordinatesRank()
 
-    rank.calc_score(5)
+    print(rank._subordinates_rank)
+
+    # rank.calc_score(5)
+
+    rank.calc_score(1)
+
+    # rank.calc_score(7)
+
+    print(rank._subordinates_rank)
+
+    # print(f'Funcionários: {rank.personnel}')
 
 
-    print(rank._subordinates)
+    # rank.add_subordinate(15, 3)
+
+    # print(rank._subordinates_rank)
+    
+    # rank.add_subordinate(8, 3)
+
+    # print(rank._subordinates_rank)
+
+    # rank.add_subordinate(9, 3)
+
+    # print(rank._subordinates_rank)
+
+    # rank.add_subordinate(10, 3)
+
+    # print(rank._subordinates_rank)
+
+    # rank.add_subordinate(19, 3)
+
+    # print(rank._subordinates_rank)
+
+    # rank.add_subordinate(20, 3)
+
+    # print(rank._subordinates_rank)
+    
+    # rank.add_subordinate(26, 3)
+
+    # print(rank._subordinates_rank)
+
+    # print(f'Funcionários: {rank.personnel}')
