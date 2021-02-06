563e6476994677f04cc70ea6954bf0fd66f55108
Mateus Talles <mateustalles@gmail.com>
2021-02-06 16:48:56 -0300

    studying remove-fimove first method

---
+++ nodes/linked_list_content.py
@@ -0,0 +1,52 @@
+class LinkedList:
+    def __init__(self):
+        self.head_value = None
+        self.__length = 0
+
+    def __str__(self):
+        return f"LinkedList(len={self.__length}, value={self.head_value})"
+
+    def __len__(self):
+        return self.__length
+
+    def is_empty(self):
+        if self.__length == 0:
+            return True
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        first_value.next = self.head_value
+        self.head_value = first_value
+        self.__length += 1
+
+    def insert_last(self, value):
+        last_value = Node(value)
+        current_value = self.head_value
+
+        if self.is_empty():
+            return self.insert_first(value)
+
+        while current_value.next:
+            current_value = current_value.next
+        current_value.next = last_value
+        self.__length += 1
+
+    def insert_at(self, value, position):
+        if position < 1:
+            return self.insert_first(value)
+        if position >= len(self):
+            return self.insert_last(value)
+        current_value = self.head_value
+        while position > 1:
+            current_value = current_value.next
+            position -= 1
+        next_value = Node(value)
+        next_value.next = current_value.next  #aqui pego a referencia mais atual e tranfiro para o novo item
+        current_value.next = next_value  #aqui transfiro a referencia do antigo item mais novo para o atual
+        self.__length += 1
+
+    def remove_first(self):
+        value_to_be_removed = self.head_value
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
---
+++ nodes/node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, value):
+        self.value = valueself.next = None
+
+    def __str__(self):
+        return f"Node(value={self.value}, next={self.next}"
